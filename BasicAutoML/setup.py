de0e36707210458c5be0e73e98948bc647186cc7
tcabgom <tcabgom222@gmail.com>
2025-08-04 18:47:01 +0200

    feat: Implemented setup.py

---
+++ BasicAutoML/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BasicAutoML',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    install_requires=[
+        'scikit-learn>=1.0',
+        'numpy>=1.21',
+        'pandas>=1.3',
+        'scipy>=1.7',
+        'joblib>=1.0',
+        'optuna>=2.10',
+        'imbalanced-learn>=0.8'
+    ],
+)
